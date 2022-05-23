f203d868bfa402232054501bed1e87b8151e382c
Sarvesh Adhari <92219557+SarveshAdhari@users.noreply.github.com>
2022-05-23 16:34:33 +0530

    Update dbConnect.js

--- crud/utils/dbConnect.js
+++ crud/utils/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 async function dbConnect(){
     try{
-        await mongoose.connect('mongodb+srv://root:<password>@cluster0.5yfz6.mongodb.net/crud',{
+        await mongoose.connect('mongodb+srv://root:5785@cluster0.5yfz6.mongodb.net/crud',{
             useUnifiedTopology: true,
             useNewUrlParser: true
         })
